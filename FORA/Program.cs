cd4c46501f1439650b4523092671d32ae6697e63
Владислав Логай <16.fi.v.lohai@std.npu.edu.ua>
2020-04-11 19:00:48 +0300

    Delete of comments

--- FORA/Program.cs
+++ FORA/Program.cs
@@ -59,48 +59,5 @@ namespace FORA
 
 
         }
-
-        //static List<Product> ListOfProduct(int countOfMilk, int countOfBread, int countOfCake, int countOfChocolate, int countOfMeat, int countOfSausage, int countOfToiletPaper)
-        //{
-        //    List<Product> list = new List<Product>();
-        //    Random random = new Random();
-        //    for (int i = 0; i < countOfMilk; i++)
-        //    {
-        //        list.Add(new Milk());
-        //    }
-
-        //    for (int i = 0; i < countOfBread; i++)
-        //    {
-        //        list.Add(new Bread());
-        //    }
-
-        //    for (int i = 0; i < countOfCake; i++)
-        //    {
-        //        list.Add(new Cake());
-        //    }
-
-        //    for (int i = 0; i < countOfChocolate; i++)
-        //    {
-        //        list.Add(new Chocolate());
-        //    }
-
-        //    for (int i = 0; i < countOfMeat; i++)
-        //    {
-        //        list.Add(new Meat());
-        //    }
-
-        //    for (int i = 0; i < countOfSausage; i++)
-        //    {
-        //        list.Add(new Sausage());
-        //    }
-
-        //    for (int i = 0; i < countOfToiletPaper; i++)
-        //    {
-        //        list.Add(new ToiletPaper());
-        //    }
-
-        //    return list;
-        //}
-
     }
 }
